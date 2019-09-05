3c4b013e37a22e980a318d08e22e826019f281be
weimeilin79 <weimielin@gmail.com>
2019-09-05 12:19:41 -0400

    Change to AMQP for AMQ Online

--- projects/module-0-eventdriven/common/ShippingService.java
+++ projects/module-0-eventdriven/common/ShippingService.java
@@ -1,17 +1,18 @@
 /** 
 kamel run --name=shipping-service -d camel-swagger-java -d camel-jackson -d camel-undertow -d mvn:org.apache.activemq:activemq-camel:5.15.9 -d mvn:org.apache.activemq:activemq-client:5.15.9 ShippingService.java
-
+kamel run --name=shipping-service -d camel-swagger-java -d camel-jackson -d camel-undertow -d camel-ahc-ws -d mvn:org.apache.activemq:activemq-camel:5.15.9 -d mvn:org.apache.activemq:activemq-client:5.15.9 ShippingService.java --dev
+kamel run --name=shipping-service -d camel-swagger-java -d camel-jackson -d camel-undertow -d camel-ahc-ws -d camel-amqp  ShippingService.java
 */
 import java.util.HashMap;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
-import org.apache.activemq.camel.component.ActiveMQComponent;
+import org.apache.camel.component.amqp.AMQPComponent;
 import org.apache.camel.component.jackson.JacksonDataFormat;
 
 public class ShippingService extends RouteBuilder {
 
-    //String BROKER_URL = "tcp://broker-amq-tcp.modulezero.svc:61616";
+    String BROKER_URL = "amqp://messaging-7rm3r5j1m3.workshop-operators.svc:5672";
     
     
     @Override
@@ -30,7 +31,7 @@ public class ShippingService extends RouteBuilder {
             .post("/notify/order")
                 .to("direct:notify");
 
-       // getContext().addComponent("activemq", ActiveMQComponent.activeMQComponent(BROKER_URL));
+        getContext().addComponent("activemq", AMQPComponent.amqpComponent(BROKER_URL));
         JacksonDataFormat jacksonDataFormat = new JacksonDataFormat();
         jacksonDataFormat.setUnmarshalType(Order.class);
         
@@ -40,13 +41,12 @@ public class ShippingService extends RouteBuilder {
             .log("Shipping Notified ${body}")
             .to("ahc-ws://dilii-ui:8181/echo?sendToAll=true")
             ;
-    /** 
-        from("activemq:topic:incomingorders?username=amq&password=password")
+        
+        from("amqp:topic://incomingorders?subscriptionDurable=false")
             .marshal(jacksonDataFormat)
-            .log("Shipping Notified ${body}")
-            .to("ahc-ws://dilii-ui:8181/echo?sendToAll=true")
+            .log("Sales Notified ${body}")
+            //.to("ahc-ws://dilii-ui:8181/echo?sendToAll=true")
             ;
-            */
     }
 
 
