f453ba1ad8faabcdd11ef2e41288826f4f43d3f7
Hugo Guerrero <1001939+hguerrero@users.noreply.github.com>
2019-10-31 18:41:49 -0400

    spring-based service

---
+++ projects/module-0-orders-service/src/main/java/com/legacy/ordersservice/OrdersRepository.java
@@ -0,0 +1,24 @@
+package com.legacy.ordersservice;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestOperations;
+
+@Repository
+public class OrdersRepository {
+    private final RestOperations restOperations;
+
+    public OrdersRepository(RestTemplateBuilder restTemplateBuilder) {
+        this.restOperations = restTemplateBuilder.build();
+    }
+
+    public String placeOrder(Order order) {
+        String res1 = this.restOperations.postForObject("http://inventory-service-myproject.192.168.99.106.nip.io/notify/order",order, String.class);
+        String res2 = this.restOperations.postForObject("http://invoice-service-myproject.192.168.99.106.nip.io/notify/order", order, String.class);
+        return createObject(res1, res2);
+    }
+
+    private String createObject(String o1, String o2) {
+        return "{\"inventory\":"+ o1 + ",\"invoice\":" + o2 + "}";
+    }
+}
