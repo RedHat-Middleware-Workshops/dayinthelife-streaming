d355c57f7727e49bcf057a56c2123fa370d8020b
Hugo Guerrero <1001939+hguerrero@users.noreply.github.com>
2019-12-13 16:08:59 -0500

    add websockets notifications

---
+++ projects/module-0-ui-mobile-new/public/js/config/mq_config.js
@@ -0,0 +1,6 @@
+var config = {
+    mq_username : "user",
+    mq_password : "password",
+    mq_server: "wss://messaging-wss-siu6nrccvk-workshop-operators.apps.cluster-eventing-749c.eventing-749c.example.opentlc.com:443",
+    mq_destination: "notifications"
+};
