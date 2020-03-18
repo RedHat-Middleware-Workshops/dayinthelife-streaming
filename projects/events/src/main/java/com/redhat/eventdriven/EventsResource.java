9090c76bb7d8f84ed2c9ca95f902c72d80220268
Hugo Guerrero <1001939+hguerrero@users.noreply.github.com>
2020-03-18 19:50:07 -0400

    Add Events project

---
+++ projects/events/src/main/java/com/redhat/eventdriven/EventsResource.java
@@ -0,0 +1,26 @@
+package com.redhat.eventdriven;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.reactivex.Flowable;
+import io.smallrye.reactive.messaging.annotations.Channel;
+
+@Path("/")
+public class EventsResource {
+
+    Logger log = LoggerFactory.getLogger(EventsResource.class);
+
+    /* TODO add notifications Channel */
+
+
+    /* TODO add consume Path */
+
+}
