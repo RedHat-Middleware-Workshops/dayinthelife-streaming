11301c19dbed0825f488e47649dd01edf564330f
weimeilin79 <weimielin@gmail.com>
2020-03-23 11:06:40 -0400

    User Projects for Module 4

---
+++ projects/module-4-cep-es/task1-costcenter/CostAdvice.java
@@ -0,0 +1,83 @@
+package module4;
+import org.apache.camel.builder.RouteBuilder;
+import java.util.Map;
+import java.util.HashMap;
+import org.apache.camel.component.jackson.JacksonDataFormat;
+import org.apache.camel.Body;
+import org.apache.camel.model.rest.RestBindingMode;
+
+//oc delete cm costadvice-config  
+//oc apply -f costadvice-config.yaml
+// 
+//kamel run -d camel-bean -d camel-jackson -d camel-swagger-java -d camel-undertow  --configmap costadvice-config CostAdvice.java --dev
+//--trait debug.enabled=true --property logging.level.org.apache.camel=DEBUG
+
+public class CostAdvice extends RouteBuilder{
+
+    
+    static Map<Integer, Double> COST_FACTOR = new HashMap<Integer, Double>();
+    static{
+        COST_FACTOR.put(101, 8.0);
+        COST_FACTOR.put(302, 5.4);
+        COST_FACTOR.put(787, 7.9);
+        COST_FACTOR.put(645, 8.7);
+        COST_FACTOR.put(555, 8.7);
+        COST_FACTOR.put(460, 7.6);
+        COST_FACTOR.put(892, 7.5);
+    }
+    
+    Map<Integer, Double> farmCost = new HashMap<Integer, Double>();
+  
+    @Override
+    public void configure() throws Exception{
+
+
+        restConfiguration()
+            .component("undertow")
+            .apiContextPath("/api-doc")
+            .apiProperty("cors", "true")
+            .apiProperty("api.title", "Order API")
+            .apiProperty("api.version", "1.0")
+            .enableCORS(true)
+            .port("8080")
+            .bindingMode(RestBindingMode.json);
+
+        rest()
+            .get("/costadvice")
+                .to("direct:costadvice")
+        ;
+
+        from("direct:costadvice")
+            .bean(this, "getCostAdvice")
+            .log("--> ${body}")
+        ;
+
+        from("kafka:user1-costcenter?groupId=costadvisor")
+            .unmarshal(new JacksonDataFormat(Map.class))
+            .bean(this, "calculate")
+        ;
+
+    }
+
+
+    public Map<Integer, Double> getCostAdvice(){
+        return farmCost;
+    }
+       
+
+
+    public void calculate(@Body Map<Integer, Integer> input){
+        int farmid = input.get("farmid");
+        double batchcost = input.get("batchcnt") * COST_FACTOR.get(farmid);
+
+        if(farmCost.get(farmid) != null && farmCost.get(farmid) >0)
+            farmCost.put(farmid, farmCost.get(farmid)+batchcost);
+        else 
+            farmCost.put(farmid, batchcost);
+
+        
+    }
+        
+
+        
+}
---
+++ projects/module-4-cep-es/task2-cep/src/main/java/com/redhat/workshop/dil/HarvestinFive.java
@@ -0,0 +1,24 @@
+package com.redhat.workshop.dil;
+
+public class HarvestinFive {
+
+    Integer totalCnt;
+    boolean isDisaster;
+
+    public Integer getTotalCnt() {
+        return totalCnt;
+    }
+
+    public void setTotalCnt(Integer totalCnt) {
+        this.totalCnt = totalCnt;
+    }
+
+    public boolean isDisaster() {
+        return isDisaster;
+    }
+
+    public void setDisaster(boolean isDisaster) {
+        this.isDisaster = isDisaster;
+    }
+
+}
