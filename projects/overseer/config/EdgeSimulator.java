514291fd4dd09b7b540ab2c2284e6db48ca2d3ea
weimeilin79 <weimielin@gmail.com>
2020-08-07 00:24:31 -0400

    Modifying to automate Pre-req

---
+++ projects/overseer/config/EdgeSimulator.java
@@ -0,0 +1,65 @@
+// camel-k: dependency=camel-bean dependency=camel-jackson configmap=edge-config
+import org.apache.camel.builder.RouteBuilder;
+import java.util.Random;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+
+
+//oc create configmap edge-config  --from-file=edge.properties
+public class EdgeSimulator extends RouteBuilder{
+    
+    @Override
+    public void configure() throws Exception{
+
+        
+
+        from("timer:tick?fixedRate=true&period=5000")
+        .choice()
+            .when(simple("{{simulator.run}}"))
+                .setBody(method(this, "genRandomIoTData()"))
+                .marshal().json()
+                .log("${body}")
+                .to("amqp:topic:mytopic?subscriptionDurable=false&exchangePattern=InOnly")
+            .otherwise()
+                .log("Nothing send ")
+        ; 
+    }
+
+
+    public Map genRandomIoTData(){
+        Random generator = new Random(); 
+        Map iotData = new HashMap<String,Object>();
+        
+        Integer[] farms = {101, 302, 787, 645, 555, 460, 892};
+        int randomIndex = generator.nextInt(farms.length);
+        int batchcnt =  generator.nextInt(87) + 1;
+        long batchtime = System.currentTimeMillis();
+
+        List<Map> harvest = new ArrayList<Map>();
+
+        for (int i = 0; i < batchcnt; i++) {
+            harvest.add(genSingleEvent((batchtime*10)+i));
+        }
+
+        iotData.put("farmid", farms[randomIndex]);
+        iotData.put("batchcnt", batchcnt);
+        iotData.put("harvest", harvest);
+        iotData.put("batchtime", batchtime);
+
+        return iotData;
+    }
+
+    private Map genSingleEvent(long eventid){
+
+        Map harvestEvent = new HashMap<String,Integer>();
+        Random generator = new Random();
+        harvestEvent.put("diameter", (generator.nextInt(5) + 1));
+        harvestEvent.put("weight", ((generator.nextInt(5) + 1.0) + (((int)generator.nextInt(8)+1)*0.1)));
+        harvestEvent.put("mmid", eventid);
+        
+        return harvestEvent;
+    }
+
+}
---
+++ projects/overseer/config/prereq.sh
@@ -0,0 +1,7 @@
+#Create the pre req AMQ online first, make sure it's running (Better for user manual check)
+#cd /projects/FleurDeLune/projects/overseer/config
+#oc apply -f prereq.yaml
+AMQHOST=$(oc get addressspace amq -o yaml | grep serviceHost | sed "s/serviceHost://g" | tr -d "[:blank:]")
+sed "s/REPLACE/$AMQHOST/g" edge.properties.bak > edge.properties
+oc create configmap edge-config  --from-file=edge.properties
+kamel run EdgeSimulator.java
