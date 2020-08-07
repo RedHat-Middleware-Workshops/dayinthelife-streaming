c5c337e4aee3009ddfaa719f50dc4ad35e5bdabd
weimeilin79 <weimielin@gmail.com>
2020-08-07 18:42:42 -0400

    update simulator to 150 max

--- projects/overseer/config/EdgeSimulator.java
+++ projects/overseer/config/EdgeSimulator.java
@@ -1,4 +1,7 @@
 // camel-k: dependency=camel-bean dependency=camel-jackson configmap=edge-config
+
+package module3.simulator;
+
 import org.apache.camel.builder.RouteBuilder;
 import java.util.Random;
 import java.util.HashMap;
@@ -6,35 +9,32 @@ import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
 
-
-//oc create configmap edge-config  --from-file=edge.properties
 public class EdgeSimulator extends RouteBuilder{
-    
-    @Override
-    public void configure() throws Exception{
 
-        
+  public static final int MIN = 150;
+  @Override
+  public void configure() throws Exception{
 
-        from("timer:tick?fixedRate=true&period=5000")
-        .choice()
-            .when(simple("{{simulator.run}}"))
-                .setBody(method(this, "genRandomIoTData()"))
-                .marshal().json()
-                .log("${body}")
-                .to("amqp:topic:mytopic?subscriptionDurable=false&exchangePattern=InOnly")
-            .otherwise()
-                .log("Nothing send ")
-        ; 
-    }
+  from("timer:tick?fixedRate=true&period=5000")
+.choice()
+    .when(simple("{{simulator.run}}"))
+        .setBody(method(this, "genRandomIoTData()"))
+        .marshal().json()
+        .log("${body}")
+        .to("amqp:topic:mytopic?subscriptionDurable=false&exchangePattern=InOnly")
+    .otherwise()
+        .log("Nothing send ")
+;
 
+  }
 
     public Map genRandomIoTData(){
-        Random generator = new Random(); 
+        Random generator = new Random();
         Map iotData = new HashMap<String,Object>();
-        
+
         Integer[] farms = {101, 302, 787, 645, 555, 460, 892};
         int randomIndex = generator.nextInt(farms.length);
-        int batchcnt =  generator.nextInt(87) + 1;
+        int batchcnt =  generator.nextInt(87) + MIN;
         long batchtime = System.currentTimeMillis();
 
         List<Map> harvest = new ArrayList<Map>();
@@ -58,7 +58,7 @@ public class EdgeSimulator extends RouteBuilder{
         harvestEvent.put("diameter", (generator.nextInt(5) + 1));
         harvestEvent.put("weight", ((generator.nextInt(5) + 1.0) + (((int)generator.nextInt(8)+1)*0.1)));
         harvestEvent.put("mmid", eventid);
-        
+
         return harvestEvent;
     }
 
